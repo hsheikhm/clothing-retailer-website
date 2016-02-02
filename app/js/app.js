ac5cb3eff7e919b9e53a3969b18ab5cd6594c592
Hamza Sheikh <hsheikhm@gmail.com>
2016-02-02 16:44:08 +0000

    completed route configuration in app.js

---
+++ app/js/app.js
@@ -0,0 +1,21 @@
+var clothingApp = angular.module('clothingApp', [
+  'ngRoute',
+  'ngResource',
+  'clothingAppControllers',
+  'clothingAppServices'
+]);
+
+clothingApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'partials/home-page.html'
+      }).
+      when('/home/:category', {
+        templateUrl: 'partials/category-page.html'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }
+]);
