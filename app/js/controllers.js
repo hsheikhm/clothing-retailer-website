38932a83c487c7d1daca61d38036d813b5a9b9a1
Hamza Sheikh <hsheikhm@gmail.com>
2016-02-02 23:04:42 +0000

    created a customer controller and shopping cart model. User can now add a product into the cart.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,18 @@ var clothingAppControllers = angular.module('clothingAppControllers', []);
 clothingAppControllers.controller('CategoryCtrl', ['$routeParams', 'Products',
   function($routeParams, Products) {
     var self = this;
-
     Products.getList($routeParams.category).then(function(category){
       self.products = category.data.products;
     });
-
   }
 ]);
+
+clothingAppControllers.controller('CustomerCtrl',
+  function() {
+    var self = this;
+    self.shoppingCart = [];
+    self.addToCart = function(product){
+      self.shoppingCart.push(product);
+    };
+  }
+);
