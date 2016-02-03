e3c2ea4884ce939979591668b85e17f8478be1ec
Hamza Sheikh <hsheikhm@gmail.com>
2016-02-03 00:49:21 +0000

    user can now remove a product from their shopping cart which also increases the product's quantity by 1

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,20 +2,33 @@ var clothingAppControllers = angular.module('clothingAppControllers', []);
 
 clothingAppControllers.controller('CategoryCtrl', ['$routeParams', 'Products',
   function($routeParams, Products) {
+
     var self = this;
+
     Products.getList($routeParams.category).then(function(category){
       self.products = category.data.products;
     });
+
   }
 ]);
 
 clothingAppControllers.controller('CustomerCtrl',
   function() {
+
     var self = this;
+
     self.shoppingCart = [];
+
     self.addToCart = function(product){
       self.shoppingCart.push(product);
       product.quantity -= 1;
     };
+
+    self.removeFromCart = function(product){
+      position = self.shoppingCart.indexOf(product);
+      self.shoppingCart.splice(position, 1);
+      product.quantity += 1;
+    };
+
   }
 );
